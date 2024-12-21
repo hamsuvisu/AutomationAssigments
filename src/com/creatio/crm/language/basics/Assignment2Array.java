893421d6655aa3a246ece952220ff58240527fe3
hamsaveni <hamsuvisu@gmail.com>
2024-12-21 16:42:36 +0530

    Assigment 2- Arrays

---
+++ src/com/creatio/crm/language/basics/Assignment2Array.java
@@ -0,0 +1,28 @@
+package com.creatio.crm.language.basics;
+
+public class Assignment2Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String [] empname = new String [2];
+		empname [0]= "hari";
+		empname [1]= "rishu";
+		
+				
+		
+	String [] [] [] products = new String [2] [4] [5];
+	products[0][0][0] = "Cola";
+	products[0][0][1] = "Orange Juice";
+	products[0][1][0] = "Popcorn";
+	products[0][2][1] = "Heavy Cream";
+	products[1][0][0] = "Cola";
+	products[1][3][0] = "Apples";
+	products[1][2][1] = "Heavy Cream";
+	
+	System.out.println(products[1][3][0]);
+	
+		
+	}
+
+}
